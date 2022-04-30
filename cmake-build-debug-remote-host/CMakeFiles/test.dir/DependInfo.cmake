a9fb0c1ab575b63cf03616128167caf126375ace
lyl010221 <750697268@qq.com>
2022-04-30 21:42:31 +0800

    add new file

---
+++ cmake-build-debug-remote-host/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Code/OpenVINO_Cpp/main.cpp" "/home/pi/Code/OpenVINO_Cpp/cmake-build-debug-remote-host/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  "/opt/intel/openvino_2021/deployment_tools/inference_engine/include"
+  "/opt/intel/openvino_2021/deployment_tools/ngraph/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
